46613458003ebfcd213f9d90cedb02038f7ddfdd
Ewan Marshall <marshall.ewan@gmail.com>
2013-07-16 19:42:22 +0000

    added stylings as in example

--- web.js
+++ web.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var fs = require('fs');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	var dataRead = fs.readFileSync('index.html');
+	var dataRead = fs.readFileSync(htmlfile);
 	var buff = new Buffer(dataRead);
 	response.send(buff.toString('utf-8'));
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
